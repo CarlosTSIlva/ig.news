cd76b96037e68b6a5a1f05477b1b456870554e94
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2021-05-09 16:25:37 -0300

    pagamentos com stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import style from "./styles.module.scss";
 
 interface SubescribeButtonProps {
@@ -5,5 +8,27 @@ interface SubescribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubescribeButtonProps) {
-  return <button className={style.subscribeButton}>Subscribe now</button>;
+  const [session] = useSession();
+
+  async function hancleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId: sessionId.id });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  return (
+    <button onClick={hancleSubscribe} className={style.subscribeButton}>
+      Subscribe now
+    </button>
+  );
 }
