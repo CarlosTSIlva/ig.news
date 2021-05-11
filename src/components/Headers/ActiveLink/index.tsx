2807863aa2c7986a518ca0d621b33df4c98a0d69
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2021-05-10 22:47:18 -0300

    trabalhando com cms

---
+++ src/components/Headers/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/dist/client/router";
+import Link, { LinkProps } from "next/link";
+import { ReactElement, cloneElement } from "react";
+
+interface ActiviveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiviveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
